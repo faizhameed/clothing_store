a3d41de53466e1ddce448502bf33c242ce0ed742
faizhameed <faizftb@gmail.com>
2019-08-01 16:51:44 +0530

    updating our app with collection item, collection preview component and several stylings realted is added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
 import {Route,Switch} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component';
 
 
-const HatsPage =()=>(<>
 
-<h2>Hats Page</h2>
-<p>this is a hats page</p>
-</>
-)
 
 function App() {
   return (
     <Switch>
       <Route exact path = '/' component = {Homepage}/>
-      <Route path = '/hats' component = {HatsPage}/>
+      <Route path = '/shop' component = {ShopPage}/>
     </Switch>
   );
 }
