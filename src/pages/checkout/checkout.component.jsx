48410650609f072ab993c6073f6f20b866024506
faizhameed <faizftb@gmail.com>
2019-09-16 11:37:21 +0530

    updating strip integration

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import {createStructuredSelector} from 'reselect'
  
 import {selectCartItems, selectCartItemsTotal} from '../../redux/cart/cart.selectors'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 const CheckoutPage = ({cartItems,cartTotal}) => {
     return (
@@ -34,6 +35,11 @@ const CheckoutPage = ({cartItems,cartTotal}) => {
             <div className='total'>
                 <span>TOTAL: ${cartTotal}</span>
             </div>
+            <div className='test-warning'>
+                *Please use the following card for payment <br/>
+                4242 4242 4242 4242 - exp 01/20 - CVV: 123
+            </div>
+            <StripeCheckoutButton price={cartTotal} />
         </div>
     )
 }
