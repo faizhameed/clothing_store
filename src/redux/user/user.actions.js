44158518786fb64ac93d47c26008cb291330878a
faizhameed <faizftb@gmail.com>
2019-11-19 10:34:33 +0530

    created user cart sagas for sign in sign up signout and with google

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -27,11 +27,25 @@ export const signOutStart = () => ({
   type: UserActionTypes.SIGN_OUT_START
 });
 export const signOutSuccess = () => ({
-  type: UserActionTypes.SIGN_IN_SUCCESS
+  type: UserActionTypes.SIGN_OUT_SUCCESS
 });
 
 export const signOutFailure = error => ({
   type: UserActionTypes.SIGN_OUT_FAILURE,
+  payload: error
+});
+
+export const signUpStart = userCredentials => ({
+  type: UserActionTypes.SIGN_UP_START,
+  payload: userCredentials
+});
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+  type: UserActionTypes.SIGN_UP_SUCCESS,
+  payload: { user, additionalData }
+});
+export const signUpFailure = error => ({
+  type: UserActionTypes.SIGN_UP_FAILURE,
   payload: error
 });
 /* 
