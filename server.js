309802a4aecbab3bcf53d8afe7abcc18ca0cb18b
faizhameed <faizftb@gmail.com>
2019-12-29 10:36:26 +0530

    backend for stripe

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express"); // express frame work as middleware to connect to our backend
+
+const cors = require("cors"); // cross orign assess which helps to make the frontend to connect from different orign to the backend hosted at different orign
+
 const bodyParser = require("body-parser");
 const path = require("path");
 
 if (process.env.NODE_ENV != "production") require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// we need to add secret key from process .env
 
 const app = express(); // intans new express appl
 const port = process.env.PORT || 5000;
@@ -11,7 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(
   bodyParser.json()
 ); /* any of the requests coming in
- I want you to process their body tag and convert it to Jason so we can
+ I want you to process their body tag and convert it to JSON so we can
 use it. */
 app.use(
   bodyParser.urlencoded({ extended: true })
@@ -32,4 +37,16 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, error => {
   if (error) throw error;
   console.log(`Server running on Port: ${port}`);
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd"
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) res.status(500).send({ error: stripeErr });
+    else res.status(200).send({ success: stripeRes });
+  });
 });
