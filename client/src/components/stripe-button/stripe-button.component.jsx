1161f639d30b851b0b6a230223cd43e6982b261a
faizhameed <faizftb@gmail.com>
2019-12-29 11:48:43 +0530

    successfully integrated strip backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_Vhz7f68pU6kBbssfHTbHxP4l00UtkQnmeg";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Succesful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment was successfull");
+      })
+      .catch(error => {
+        console.log("payment error", JSON.parse(error));
+        alert(
+          "there was an issue with your payment,please make sure you use the provided credit card"
+        );
+      });
   };
 
   return (
